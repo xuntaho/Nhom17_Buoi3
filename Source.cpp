72d72638276f0ff621d8f2e9f1136c90ba94be7b
Pham Phong Nha <nha158400@gmail.com>
2025-10-27 11:01:33 +0700

    Tong

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b)
+{
+	cout<<"Nhap so a : ";
+	cin>>a;
+	cout<<"/nNhap so a : ";
+	cin>>b;
+	return a+b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a=0;
+	int b=0;
+	cout<<"Tong la "<<tong(a,b);
 	system("pause");
 	return 0;
 }
