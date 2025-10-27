f798a7806ccfefd76381370f5145e03a7f24c0d2
Nguyen Ai Phuong Thao <aithaodoris123@gmai.com>
2025-10-27 11:11:37 +0700

    hieu

--- Source.cpp
+++ Source.cpp
@@ -2,12 +2,25 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	cout << "Nhap a ";
+	cin >> a;
+	cout << "Nhap b ";
+	cin >> b;
+	
+	return a-b;
+	
+}
+
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a =0;
+	int b =0;
+	cout << hieu(a, b);
 
 	system("pause");
 	return 0;
