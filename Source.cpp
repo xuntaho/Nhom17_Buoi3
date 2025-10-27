f7efd2a523f3112ca5e7c9dd57c992851fde1d1f
Vo Thi Xuan Thao <xuanthao943@gmail.com>
2025-10-27 10:33:16 +0700

    init

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int tong(int a, int b);
+int hieu(int a, int b);
+int tich(int a, int b);
+float thuong(int a, int b);
+int main()
+{
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+
+	system("pause");
+	return 0;
+}
